088e87c05525f1153217f5059aa689bb7bf74c42
Rajesh Pillai <pillai.rajesh@gmail.com>
2021-08-11 11:02:57 +0530

    Read Post view and route

--- lib/lv_demo_web/live/blog_live/read.ex
+++ lib/lv_demo_web/live/blog_live/read.ex
@@ -1,6 +1,6 @@
 # https://minhajuddin.com/2020/05/03/many-to-many-relationships-in-ecto-and-phoenix-for-products-and-tags/
 
-defmodule LvDemoWeb.BlogLive.Show do
+defmodule LvDemoWeb.BlogLive.Read do
   use LvDemoWeb, :live_view
 
   alias LvDemo.Blogs
@@ -13,7 +13,7 @@ defmodule LvDemoWeb.BlogLive.Show do
 
   # Post Edit
   def handle_params(%{"post_id" => post_id, "id" => id} = params, _, socket) do
-    IO.puts "+++EDIT POST-----"
+    IO.puts "+++READ POST-----"
     IO.inspect params
     post = Blogs.get_post!(post_id)
     IO.inspect post
@@ -25,22 +25,6 @@ defmodule LvDemoWeb.BlogLive.Show do
   end
 
 
-  @impl true
-  def handle_params(%{"id" => id} = params, _, socket) do
-    IO.puts "+++DEFAULT NEW POST-----"
-    IO.inspect params
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:post, %Post{blog_id: id, tags: []})
-     |> assign(:blog, Blogs.get_blog!(id))}
-  end
+  defp page_title(:read_post), do: "Read Post"
 
-
-
-  defp page_title(:new_post), do: "New Post"
-  defp page_title(:edit_post), do: "Edit Post"
-
-  defp page_title(:show), do: "Show Blog"
-  defp page_title(:edit), do: "Edit Blog"
 end
--- lib/lv_demo_web/router.ex
+++ lib/lv_demo_web/router.ex
@@ -26,9 +26,11 @@ defmodule LvDemoWeb.Router do
     live "/blogs/:id/edit", BlogLive.Index, :edit
     live "/blogs/:id", BlogLive.Show, :show
     live "/blogs/:id/show/edit", BlogLive.Show, :edit
-    
+
     live "/blogs/:id/new", BlogLive.Show, :new_post
+
     live "/blogs/:id/edit/:post_id", BlogLive.Show, :edit_post
+    live "/blogs/:id/read/:post_id", BlogLive.Read, :read_post
 
 
     live "/posts", PostLive.Index, :index
